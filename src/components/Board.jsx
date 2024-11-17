d82c7697e597927148b19ec0072e70da0f006c4e
Saritha Chinthapalli <saritha@snovasys.com>
2024-11-17 15:45:38 +0530

    changes

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -8,23 +8,23 @@ import Tasks from './Tasks';
 import nodataimag from '../assets/nodataimag.jpg'
 export default function Board() {
   const [addProjectPopup, openAddProjectPopup] = useState(false)
-  const [addMemberPopup,openAddMemberPopup] = useState(false)
-  const [currentProject,setCurrentProject]=useState(null)
-  const [projects,updateProjects] = useState([])
-  const [taskpage,openTaskPage] = useState(false)
-  const [id,setId]=useState(null)
-  const [row,setRow]=useState(null)
-  const projectsData= async ()=>{
+  const [addMemberPopup, openAddMemberPopup] = useState(false)
+  const [currentProject, setCurrentProject] = useState(null)
+  const [projects, updateProjects] = useState([])
+  const [taskpage, openTaskPage] = useState(false)
+  const [id, setId] = useState(null)
+  const [row, setRow] = useState(null)
+  const projectsData = async () => {
     const response = await axios.get(`${import.meta.env.VITE_API_URL}/api/board`)
     updateProjects(response.data.projects)
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     projectsData()
-  },[])
+  }, [])
 
   const addPorject = async (projectName) => {
-    
+
     try {
       const response = await axios.post(`${import.meta.env.VITE_API_URL}/api/board/addProjects`, { projectName })
       if (response.data.success) {
@@ -36,13 +36,13 @@ export default function Board() {
         toast.error(e.message)
       }
     }
-    catch(e) {
+    catch (e) {
       toast.error('Failed Due To Error..')
     }
   }
 
   const editPorject = async (projectName) => {
-    
+
     try {
       const response = await axios.put(`${import.meta.env.VITE_API_URL}/api/board/updateProject/${id}`, { projectName })
       if (response.data.success) {
@@ -54,11 +54,11 @@ export default function Board() {
         toast.error(e.message)
       }
     }
-    catch(e) {
+    catch (e) {
       toast.error('Failed Due To Error..')
     }
   }
-  const deleteProject=async (id)=>{
+  const deleteProject = async (id) => {
     const response = await axios.delete(`${import.meta.env.VITE_API_URL}/api/board/deleteProject/${id}`)
     if (response.data.success) {
       toast.success('Deleted the Project Succesfully')
@@ -80,7 +80,7 @@ export default function Board() {
       name: '',
       cell: (row) => (
         <div>
-          <button className="btn mr-2" style={{backgroundColor:'rgb(54, 194, 196)',color:'white'}} onClick={()=>{openTaskPage(true);setRow(row)}}>Go To Task</button>
+          <button className="btn mr-2" style={{ backgroundColor: 'rgb(54, 194, 196)', color: 'white' }} onClick={() => { openTaskPage(true); setRow(row) }}>Go To Task</button>
         </div>
       ),
     },
@@ -89,7 +89,7 @@ export default function Board() {
       cell: (row) => (
         <div>
           <button className=" mr-2 bg-green-600 hover:bg-green-700
- text-white py-2 px-4 rounded"  onClick={()=>{openAddMemberPopup(true);setId(row._id)}}>Add Project Member</button>
+ text-white py-2 px-4 rounded"  onClick={() => { openAddMemberPopup(true); setId(row._id) }}>Add Project Member</button>
         </div>
       ),
     },
@@ -97,18 +97,18 @@ export default function Board() {
       name: 'Actions',
       cell: (row) => (
         <div>
-          <button className="btn  mr-2" style={{backgroundColor:'#f39c12',color:'white'}} onClick={()=>{setId(row._id);setCurrentProject(row.projectName);openAddProjectPopup(true)}}>Edit</button>
-          <button className="btn mr-2"  style={{backgroundColor:'#e74c3c',color:'white'}} onClick={()=>{deleteProject(row._id)}}>Delete</button>
+          <button className="btn  mr-2" style={{ backgroundColor: '#f39c12', color: 'white' }} onClick={() => { setId(row._id); setCurrentProject(row.projectName); openAddProjectPopup(true) }}>Edit</button>
+          <button className="btn mr-2" style={{ backgroundColor: '#e74c3c', color: 'white' }} onClick={() => { deleteProject(row._id) }}>Delete</button>
         </div>
       ),
     },
-    
+
   ];
   const customStyles = {
 
     headCells: {
       style: {
-       backgroundColor:'#5d6d7e',
+        backgroundColor: '#5d6d7e',
         color: 'white',
         fontSize: '16px',
         fontWeight: 'bold',
@@ -123,31 +123,31 @@ export default function Board() {
     },
   };
   return (
-    <div style={{height:"100%"}}>
-     {!taskpage && <div className='flex flex-row justify-end'><button className='bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded m-3' onClick={() => {setCurrentProject(null);openAddProjectPopup(true)}}> + Add Projects</button></div>}
+    <div style={{ height: "100%" }}>
+      {!taskpage && <div className='flex flex-row justify-end'><button className='bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded m-3' onClick={() => { setCurrentProject(null); openAddProjectPopup(true) }}> + Add Projects</button></div>}
       {
-       !taskpage && addProjectPopup && <AddProject openAddProjectPopup={openAddProjectPopup} addPorject={addPorject} currentProject={currentProject} setCurrentProject={setCurrentProject} editPorject={editPorject}/>
+        !taskpage && addProjectPopup && <AddProject openAddProjectPopup={openAddProjectPopup} addPorject={addPorject} currentProject={currentProject} setCurrentProject={setCurrentProject} editPorject={editPorject} />
       }
-      {!taskpage && projects && projects.length > 0 ? 
+      {!taskpage && projects && projects.length > 0 ?
         <div style={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
-      <DataTable
-        columns={columns}
-        data={projects}
-        pagination
-        paginationPerPage={10}
-        paginationRowsPerPageOptions={[10, 20]}
-        customStyles={customStyles}
-        striped
-        fixedHeader
-        fixedHeaderScrollHeight="calc(100vh - 200px)"
-      />
-      </div>
-     : !taskpage && projects && projects.length ==0 && <div className='flex items-center justify-center w-full' style={{height:"70vh"}}><img width="600px" height="600px" src={nodataimag}/></div>
-}
-     {
-      !taskpage && addMemberPopup && <AddProjectMember openAddMemberPopup={openAddMemberPopup} id={id}/>
-     }
-     {taskpage && <Tasks row={row} openTaskPage={openTaskPage}/>}
+          <DataTable
+            columns={columns}
+            data={projects}
+            pagination
+            paginationPerPage={10}
+            paginationRowsPerPageOptions={[10, 20]}
+            customStyles={customStyles}
+            striped
+            fixedHeader
+            fixedHeaderScrollHeight="calc(100vh - 200px)"
+          />
+        </div>
+        : !taskpage && projects && projects.length == 0 && <div className='flex items-center justify-center w-full' style={{ height: "70vh" }}><img width="600px" height="600px" src={nodataimag} /></div>
+      }
+      {
+        !taskpage && addMemberPopup && <AddProjectMember openAddMemberPopup={openAddMemberPopup} id={id} />
+      }
+      {taskpage && <Tasks row={row} openTaskPage={openTaskPage} />}
     </div>
   );
 }
